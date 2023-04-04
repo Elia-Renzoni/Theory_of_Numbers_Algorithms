6fd698d5ba27aa87eef79b25f9560dad61c289ec
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-04-04 21:18:02 +0200

    Update conjecture_resolutions.c

--- conjecture_resolutions.c
+++ conjecture_resolutions.c
@@ -1,1 +1,14 @@
-
+/*
+ *
+ *
+ *
+ *	Autori : Alessio Biagioli - Elia Renzoni
+ *
+ *	Programma : verifica delle congetture di Goldbach, Gilbreath, Legendre
+ *
+ *	PPL - Universita' Degli Studi di Urbino Carlo Bo' - Informatica Applicata L-31
+ *
+ *
+ *
+ *
+ * */
